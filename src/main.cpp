fad54a1c042e7ebb646dfbbf52dbacf2dde81a86
Andre Feofilaktov <andarpolis@gmail.com>
2016-12-18 22:42:46 +0300

    first trial usage added

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+#include <functions.h>
+#include <equation_solver.h>
+
 int main() {
+    EquationSolver s(f);
+    std::ofstream fout;
+    fout.open("data/output");
+    s.Solve2(fout, 0.0, 1.0);
+    fout.close();
+
     return 0;
 }
